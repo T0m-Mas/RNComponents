5e42fc7d817d53478922ea714a0f5d6959bd89ae
T0m-Mas <tomas_masotta@live.com>
2022-08-05 22:24:44 -0300

    ScrollFixes

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -6,27 +6,16 @@ import {
   Image,
   ScrollView,
 } from 'react-native';
+import {HeaderTitle} from '../components/HeaderTitle';
 
 export const InfiniteScrollScreen = () => {
   const imagesToLoad = 5;
   const {width} = useWindowDimensions();
 
   const load = () => {
-    let moreData: string[][] = [[], [], []];
+    let moreData: string[] = [];
     for (let i = 0; i < imagesToLoad; i++) {
-      moreData[0].push(
-        `https://picsum.photos/seed/${Math.random()}/${width / 2}/${(
-          Math.random() * (160 - 100) +
-          100
-        ).toFixed()}`,
-      );
-      moreData[1].push(
-        `https://picsum.photos/seed/${Math.random()}/${width / 2}/${(
-          Math.random() * (160 - 100) +
-          100
-        ).toFixed()}`,
-      );
-      moreData[2].push(
+      moreData.push(
         `https://picsum.photos/seed/${Math.random()}/${width / 2}/${(
           Math.random() * (160 - 100) +
           100
@@ -35,13 +24,25 @@ export const InfiniteScrollScreen = () => {
     }
     return moreData;
   };
-  const [data, setData] = useState<string[][]>(load());
-
-  // {item.map((subitem) =>
+  const [data, setData] = useState<string[][]>([load(), load(), load()]);
 
   return (
     <View style={screen}>
-      <ScrollView onScrollEndDrag={() => setData([...data, ...load()])}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        onScrollEndDrag={() =>
+          setData([
+            [...data[0], ...load()],
+            [...data[1], ...load()],
+            [...data[2], ...load()],
+          ])
+        }>
+        <View style={{margin: 5, marginBottom: 10}}>
+          <HeaderTitle
+            title="Infinite Scroll"
+            subtitle="an infinite srolling posibilities"
+          />
+        </View>
         <View style={row}>
           {data.map((item, i) => {
             return (
